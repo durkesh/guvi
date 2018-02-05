dc3f31937ae9eb758ac417e0616e7f1b19776cbe
durkesh <durkeshsr@gmail.com>
2018-02-05 16:35:52 +0530

    Create holiday.py

---
+++ holiday.py
@@ -0,0 +1,12 @@
+def main():
+   list=['monday','tuesday','wednesday','thursday','friday']
+   lis1=['sunday','saturday']
+   day=input()
+   if day in list:
+       print("no")
+   else:
+       print("yes")
+
+
+if __name__ == '__main__':
+    main()
